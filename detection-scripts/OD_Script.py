72f3983bdb927eb9b8500fd1141acb0e31e0be27
danyal-saqib <dsaqib.bee17seecs@seecs.edu.pk>
2020-12-09 19:53:46 +0500

    Improved the GUI and added a functional button for turning on and off the Object Detector

--- detection-scripts/OD_Script.py
+++ detection-scripts/OD_Script.py
@@ -27,11 +27,30 @@ net = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
 net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
 net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
-wind = ft.objWin(0, 0, '0', '0')      
-    
+wind = ft.objWin(0, 0, '0', '0')
+
+def Lolv1():
+    img = cv2.imread('object-detection-load.png')
+
+    x = 0
+    y = 0
+    a = "NONE"
+    b = "0"
+
+    cv2image = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
+    im = PIL.Image.fromarray(cv2image)
+    imTk = ImageTk.PhotoImage(im)
+    wind.lmain.imTk = imTk
+    wind.lmain.configure(image = imTk)
+    wind.newVar(x, y, a, b)
+    if wind.controlVar:
+        wind.lmain.after(1, Lol)
+    else:
+        wind.lmain.after(1, Lolv1)
+
 def Lol():
-    
     success, img = cap.read()
+    img = cv2.flip(img, 1)
     
     blob = cv2.dnn.blobFromImage(img, 1/255, (whT, whT), [0,0,0], 1, crop = False)
     net.setInput(blob)
@@ -54,7 +73,11 @@ def Lol():
     wind.lmain.imTk = imTk
     wind.lmain.configure(image = imTk)
     wind.newVar(x, y, a, b)
-    wind.lmain.after(1, Lol)
+    if wind.controlVar:
+        wind.lmain.after(1, Lol)
+    else:
+        wind.lmain.after(1, Lolv1)
+        
 
-Lol()
+Lolv1()
 wind.root.mainloop()
--- detection-scripts/functs.py
+++ detection-scripts/functs.py
@@ -7,11 +7,12 @@ from PIL import Image
 from PIL import ImageTk
 import PIL
 
-confThreshold = 0.5
+confThreshold = 0.4
 nmsThreshold = 0.3
 whT = 416
 classesFile = 'coco_names.txt'
 classNames = []
+
 
 
 def findObjects(outputs, img, classNames):
@@ -36,6 +37,7 @@ def findObjects(outputs, img, classNames):
                 confs.append(float(confidence))
     
     indices = cv2.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
+    
     for i in indices:
         i = i[0]
         box = bbox[i]
@@ -43,20 +45,22 @@ def findObjects(outputs, img, classNames):
         cv2.rectangle(img, (x, y), (x+w, y+w), (255, 0, 255), 2)
         cv2.putText(img, f'{classNames[classIds[i]].upper()} {int(confs[i]*100)}%',
                     (x, y-20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
-        a.append(f'{classNames[classIds[i]].upper()}')
-        b.append(f'{int(confs[i]*100)}%')
+        lrg = np.argmax(confs)
+        a = f'{classNames[classIds[lrg]].upper()}'
+        b = f'{int(confs[lrg]*100)}%'
     return x, y, a, b
         
 class objWin():
     def __init__(self, x, y, a, b):
         self.root = Tk()
         self.root.title("Object Detection - Camera")
+        self.root['bg'] = '#565656'
         self.root.bind('<Escape>', lambda e: root.quit())
         self.canvas1 = tk.Canvas(self.root, width = 400, height = 300,  
                                  relief = 'raised')
         self.lmain = Label(self.root, bd=2, relief=FLAT)
         self.lmain.grid(row = 0, column = 0, sticky = W, pady = 2,
-                        rowspan = 8) 
+                        rowspan = 8, columnspan = 3) 
         self.btnText = tk.StringVar()
         self.xvar = tk.StringVar()
         self.yvar = tk.StringVar()
@@ -67,45 +71,67 @@ class objWin():
         self.avar.set(a)
         self.bvar.set(b)
         
-        self.btnText.set("Click to start Object Detection")        
-        self.btn = Button(self.root, textvariable=self.btnText, 
-                          command=self.changeText, height=5, width=30)
-        self.btn.grid(row = 5, column = 4, sticky = W, pady = 2,
-                      columnspan = 2)
+        #self.btnText.set("Click to start Object Detection")
+        self.lol1 = 0        
+        self.controlVar = False
+        self.btn = Button(self.root, text="Click to start Object Detection", 
+                          command=self.changeText, height=3, width=30)
+        self.btn.config(bg = '#DCDCDC')
+        self.btn.grid(row = 7, column = 5, pady = 2,
+                      columnspan = 1)
         
-        self.labela1 = tk.Label(self.root, text = "Detected Object: ")
+        self.labela1 = tk.Label(self.root, text = "Detected Object: ",
+                                fg = 'white')
+        self.labela1.config(bg = '#565656')
         self.labela2 = tk.Label(self.root, textvariable=self.avar,
-                                 height=5, width=10)
+                                 height=2, width=15)
+        self.labela2.config(bg = 'white')
         self.labela1.grid(row = 0, column = 4, sticky = W, pady = 10) 
-        self.labela2.grid(row = 0, column = 5, sticky = W, pady = 10,
+        self.labela2.grid(row = 0, column = 6, sticky = W, pady = 10,
                           columnspan = 2) 
         
-        self.labelb1 = tk.Label(self.root, text = "Confidence (%): ")
+        self.labelb1 = tk.Label(self.root, text = "Confidence (%): ",
+                                fg = 'white')
+        self.labelb1.config(bg = '#565656')
         self.labelb2 = tk.Label(self.root, textvariable=self.bvar,
-                                 height=5, width=10)
+                                 height=2, width=15)
+        self.labelb2.config(bg = 'white')
         self.labelb1.grid(row = 1, column = 4, sticky = W, pady = 10) 
-        self.labelb2.grid(row = 1, column = 5, sticky = W, pady = 10,
+        self.labelb2.grid(row = 1, column = 6, sticky = W, pady = 10,
                           columnspan = 2) 
         
-        self.labelx1 = tk.Label(self.root, text = "X Co-ordinate: ")
+        self.labelx1 = tk.Label(self.root, text = "X Co-ordinate: ",
+                                fg = 'white')
+        self.labelx1.config(bg = '#565656')
         self.labelx2 = tk.Label(self.root, textvariable=self.xvar,
-                                height=5, width=10)
+                                height=2, width=15)
+        self.labelx2.config(bg = 'white')
         self.labelx1.grid(row = 2, column = 4, sticky = W, pady = 10) 
-        self.labelx2.grid(row = 2, column = 5, sticky = W, pady = 10,
+        self.labelx2.grid(row = 2, column = 6, sticky = W, pady = 10,
                           columnspan = 2) 
         
-        self.labely1 = tk.Label(self.root, text = "Y Co-ordinate: ")
+        self.labely1 = tk.Label(self.root, text = "Y Co-ordinate: ",
+                                fg = 'white')
+        self.labely1.config(bg = '#565656')
         self.labely2 = tk.Label(self.root, textvariable=self.yvar,
-                                 height=5, width=10)
+                                 height=2, width=15)
+        self.labely2.config(bg = 'white')
         self.labely1.grid(row = 3, column = 4, sticky = W, pady = 10) 
-        self.labely2.grid(row = 3, column = 5, sticky = W, pady = 10,
+        self.labely2.grid(row = 3, column = 6, sticky = W, pady = 10,
                           columnspan = 2) 
         
     def changeText(self):
-        if self.btnText == "Click to start Object Detection":
-            self.btnText.set("Click to stop Object Detection")
+        if self.lol1 == 0:
+            self.btn.config(text = "Click to stop Object Detection",
+                            bg = '#a9a9a9')
+            self.lol1 = 1
+            self.controlVar = True
         else:
-            self.btnText.set("Click to start Object Detection")
+            self.btn.config(text = "Click to start Object Detection",
+                            bg = '#DCDCDC')
+            self.lol1 = 0
+            self.controlVar = False
+            
     def newVar(self, x, y, a, b):
         self.xvar.set(str(x))
         self.yvar.set(str(y))
